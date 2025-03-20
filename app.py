259085076800d8f71b4599a68b31809243a88099
ryan-c123 <ryanc545@nycstudents.net>
2025-03-20 10:15:56 -0400

    ok

--- app.py
+++ app.py
@@ -10,7 +10,26 @@ def csv_to_list(file_path):
 
     return data_list
 
+### 1) Produce the Average sales data per dat for each store. Be sure to map a location to each store. 
 
+### 2) Sort the stores by most profitable. 
+
+### 3) Get The Average Sales Data of all stores
+
+### 4) Which stores are in danger of being closed (performing 20% below average sales?)
 file_path = "SalesData.csv"  
 data = csv_to_list(file_path)
-print(data)  # Output the list
+#print(data)  # Output the list
+def avg(x):
+    total = {}
+
+    for row in x[1:]:
+        name = row[0]
+        sales = map(int, row[1:])
+        total[name] = sum(sales)/30
+    
+    return total
+
+allavg = avg(data)
+
+print(allavg)
--- map.py
+++ map.py
@@ -1,4 +1,3 @@
 temperatures = ["Label", 32, 50, 77, 104]
-c = map(lambda f: (f-32)*(5/9), temperatures) 
-
+c = map(lambda f: (f-32)*(5/9), temperatures[1:]) 
 print(list(c))
